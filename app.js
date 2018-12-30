b7dd1df8496d024a46eedcdb7a5a9f5465224492
anmolmalhotra97 <anmolmalhotra97@gmail.com>
2018-12-30 19:55:26 +0530

    added google credentials and secret and made a test route to auth

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+//load routes
+const auth = require("./routes/auth");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("It works!");
 });
+
+//Use Routes
+app.use("/auth", auth);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://anmol:priya123@ds145574.mlab.com:45574/storybooks-dev",
+  googleClientID:
+    "1037301015911-rkd9kuk9vhg8hgiun4t9j3etr2obtuk3.apps.googleusercontent.com",
+  googleClientSecret: "zZYXSb_Y-xJV9r9yh5XZuACc"
+};
