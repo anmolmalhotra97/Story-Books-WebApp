8268f79a2cab38d95b983b60eba2a34a2830ea17
anmolmalhotra97 <anmolmalhotra97@gmail.com>
2019-01-01 20:01:14 +0530

    set up keys_prod,dev for depoloyment on heroku

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://anmol:priya123@ds145574.mlab.com:45574/storybooks-dev",
-  googleClientID:
-    "283653988592-6cbl6j7u193qc923v5vursi4j43754pq.apps.googleusercontent.com",
-  googleClientSecret: "f5242LAc5LeD0I7M-OmKlHAl"
-};
+if (process.env.NODE_ENV == "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
